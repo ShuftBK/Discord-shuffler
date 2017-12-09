fe9464f8804e783629da181fac60d061a90cfcb6
Shuft <shuft@coils.work>
2017-12-10 02:39:26 +0900

    add !add command and !flush command

--- shuffler.py
+++ shuffler.py
@@ -29,14 +29,27 @@ async def on_message(message):
     global map
     print("message : ",message.content)
     if message.content.startswith('!addme'):
-        messagesplit = message.content.split(' ',1)
         username = message.author.name
         level = 1
         userdict.update({username:level})
-        await client.send_message(message.channel, '{} add shuffle users list.'.format(message.author.name))
+        await client.send_message(message.channel, '{} add shuffle users list.'.format(username))
+        print(userdict)
+
+    elif message.content.startswith('!add'):
+        username = message.content.split(' ',1)[1]
+        level = 1
+        print(username)
+        userdict.update({username:level})
+        await client.send_message(message.channel, '{} add shuffle users list.'.format(username))
         print(userdict)
 
     elif message.content.startswith('!flushme'):
+        del userdict[message.author.name]
+        await client.send_message(message.channel, 'flush {} list data.'.format(message.author.name))
+        print(userdict)
+
+    elif message.content.startswith('!flush'):
+        username = message.content.split(' ',1)[1]
         del userdict[message.author.name]
         await client.send_message(message.channel, 'flush {} list data.'.format(message.author.name))
         print(userdict)
@@ -96,7 +109,7 @@ async def on_message(message):
         await client.send_message(message.channel, 'yo')
 
     elif message.content.startswith('!help'):
-        desc = "!addme\t-> add list \n!flush\t\t-> delete list \n!flushall\t-> delete all user\n!shuffle\t-> shuffle list and display result \n!showlist\t-> show user list \n\n Debug Commands\n!testdata\t-> insert test data to list"
+        desc = "!addme\t-> add list \n!flush\t\t-> delete list \n!flushall\t-> delete all user\n!map\t-> choice map\n!shuffle\t-> shuffle list and display result \n!showlist\t-> show user list \n\n Debug Commands\n!testdata\t-> insert test data to list"
         em = discord.Embed(title='Shuffler Help Command List', type="rich",description=desc, colour=0xDEADBF)
         em.set_author(name='Someone', icon_url=client.user.default_avatar_url)
         await client.send_message(message.channel, embed=em)
